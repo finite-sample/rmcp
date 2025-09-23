f5d2126c81369c9b5bfab173522149a3ac83c0d9
***** <721466+soodoku@users.noreply.github.com>
2025-09-22 19:41:34 -0700

    let's make docs consistent with readme

--- docs/conf.py
+++ docs/conf.py
@@ -29,17 +29,20 @@ except ImportError:
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
+    "sphinx.ext.autosummary", 
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx",
-    "sphinx_autodoc_typehints",
     "myst_parser",
-    "sphinx_copybutton",
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# Source file parsers
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
